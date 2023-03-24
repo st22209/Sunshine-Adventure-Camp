7c88075941bba74308785f02f8c5a9c5a31f5084
FusionSid <siddheshadsv@icloud.com>
2023-03-25 01:19:57 +1300

    feat: new record route post request route

--- api/src/routes/v1/records.py
+++ api/src/routes/v1/records.py
@@ -4,7 +4,10 @@ Endpoints for creating, deleting, updating, and getting records
 
 __all__ = ("records_endpoint",)
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+
+from core import NewRecord, Record
+
 
 records_endpoint = APIRouter(
     tags=[
@@ -12,3 +15,14 @@ records_endpoint = APIRouter(
     ],
     prefix="/api/v1/records",
 )
+
+
+@records_endpoint.post("/")
+async def create_new_record(request: Request, data: NewRecord):
+    await Record.create(
+        name=data.name,
+        location=data.location,
+        weather=data.weather,
+        camper_count=data.camper_count,
+    )
+    return {"success": True, "detail": "Record created successfully!"}
