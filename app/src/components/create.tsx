430fd9359f52b6e500cb00b4d1f9bf567fd568af
FusionSid <siddheshadsv@icloud.com>
2023-03-26 17:40:20 +1300

    feat: working create records form

--- app/src/components/create.tsx
+++ app/src/components/create.tsx
@@ -1,9 +1,109 @@
-import React from 'react'
+import { useState } from "react";
 
 const Create = () => {
-  return (
-    <div>Create</div>
-  )
-}
+	const API_URL = "http://127.0.0.1:8443/api/v1/records/";
 
-export default Create
+	const [name, setName] = useState("");
+	const [location, setLocation] = useState("");
+	const [weather, setWeather] = useState("");
+	const [count, setCount] = useState("");
+
+	let handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		let data = JSON.stringify({
+			name: name,
+			location: location,
+			weather: weather,
+			camper_count: parseInt(count),
+		});
+		try {
+			let res = await fetch(API_URL, {
+				method: "POST",
+				body: data,
+				headers: {
+					Accept: "application/json",
+					"Content-Type": "application/json",
+				},
+			});
+
+			let response = await res.json();
+			if (response["success"] !== true) {
+				console.log(response);
+				throw new Error();
+			}
+			alert("Record Created Successfuly!");
+		} catch {
+			alert(
+				"An unexpected error occured while trying to create a record please try again later"
+			);
+		}
+	};
+
+	return (
+		<div className="flex flex-col text-center items-center lg:flex">
+			<h1 className="text-red-500 text-4xl md:text-6xl  m-10 xl:text-8xl font-bold drop-shadow-xl">
+				Sunshine Adventure Camp
+			</h1>
+			<div className="bg-white w-[70%] h-[35rem] rounded-xl shadow-lg">
+				<h1 className="mt-10 text-xl">Create Record</h1>
+				<form className="mt-6" onSubmit={handleSubmit}>
+					<div className="mb-2">
+						<label className="block text-sm font-semibold text-gray-800">
+							Name
+						</label>
+						<input
+							className="px-4 py-2 mt-2 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40"
+							value={name}
+							onChange={(e) => setName(e.target.value)}
+						/>
+					</div>
+					<div className="mb-2">
+						<label className="block text-sm font-semibold text-gray-800">
+							Location
+						</label>
+						<input
+							className="px-4 py-2 mt-2 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40"
+							value={location}
+							onChange={(e) => setLocation(e.target.value)}
+						/>
+					</div>
+					<div className="mb-2">
+						<label className="block text-sm font-semibold text-gray-800">
+							Weather Conditions
+						</label>
+						<input
+							className="px-4 py-2 mt-2 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40"
+							value={weather}
+							onChange={(e) => setWeather(e.target.value)}
+						/>
+					</div>
+					<div className="mb-2">
+						<label className="block text-sm font-semibold text-gray-800">
+							Camper Count
+						</label>
+						<input
+							className="px-4 py-2 mt-2 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40"
+							value={count}
+							onChange={(e) => setCount(e.target.value)}
+							type="number"
+							min={5}
+							max={10}
+						/>
+					</div>
+
+					<div className="mt-6">
+						<button
+							type="submit"
+							className="px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-indigo-700 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600"
+						>
+							Radio In Data!
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
+};
+
+export default Create;
