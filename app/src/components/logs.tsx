a42699f29b4170b38221897917e0a78cbcee3ff0
FusionSid <siddheshadsv@icloud.com>
2023-03-27 11:49:49 +1300

    feat: add logs (no good ui yet) with 2 second long polling

--- app/src/components/logs.tsx
+++ app/src/components/logs.tsx
@@ -1,7 +1,59 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
-const Logs = () => {
-	return <div>Logs</div>;
-};
+function Logs() {
+	interface dataTypes {
+		location: string;
+		weather: string;
+		name: string;
+		camper_count: number;
+		id: string;
+		timestamp: string;
+	}
+	type dataType = Array<dataTypes>;
+	const [data, setData] = useState<dataType>([]);
+	useEffect(() => {
+		const getData = async () => {
+			const API_URL = "http://127.0.0.1:8443/api/v1/records/";
+
+			let res = await fetch(API_URL, {
+				method: "GET",
+				headers: {
+					Accept: "application/json",
+					"Content-Type": "application/json",
+				},
+			});
+			let res_data = await res.json();
+			// @ts-ignore
+			if (res_data["success"] === false) {
+				return alert("error api skill issue");
+			}
+			setData(res_data["records"]);
+		};
+		let interval = setInterval(() => {
+			getData();
+		}, 2000);
+		return () => {
+			clearInterval(interval);
+		};
+	}, []);
+	console.log(data);
+	return (
+		<div>
+			{data.map((record) => {
+				return (
+					<>
+						<p>
+							{record.timestamp} {record.name} is at{" "}
+							{record.location} with {record.camper_count}{" "}
+							campers. The weather conditions are:{" "}
+							{record.weather}
+						</p>
+						<br />
+					</>
+				);
+			})}
+		</div>
+	);
+}
 
 export default Logs;
