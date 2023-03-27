f260e0d298914d0ae537cce121b82da6ec3b106f
FusionSid <siddheshadsv@icloud.com>
2023-03-27 21:57:55 +1300

    fix: timestamp not being correct timezone/date + add option to opt out of conversion

--- api/src/routes/records.py
+++ api/src/routes/records.py
@@ -50,7 +50,9 @@ async def delete_existing_record(request: Request, record_id: int):
 
 
 @records_endpoint.get("/")
-async def get_existing_record(request: Request, record_id: Optional[int] = None):
+async def get_existing_record(
+    request: Request, record_id: Optional[int] = None, convert_timestamp: bool = False
+):
     """
     This endpoint gets and existing record from the database
     it can also get all the records from the db
@@ -61,20 +63,31 @@ async def get_existing_record(request: Request, record_id: Optional[int] = None)
         exists = await Record.exists(id=record_id)
         if not exists:  # if the record doen't exist in the db
             raise InvalidRecordID
+
         record = await Record.get(id=record_id)
+        if not convert_timestamp:  # if they chose not to convert the value
+            # return it how it is
+            return {"success": True, "record": record}
+
+        # they chose to convert it
         converted_record = (await record_pyd.from_tortoise_orm(record)).dict()
-        converted_record["timestamp"] = record.timestamp.utcnow().timestamp()
+        converted_record["timestamp"] = record.timestamp.timestamp() * 1000
 
         return {"success": True, "record": converted_record}
 
     all_db_records = await Record.all()
-    all_records = []
+    if not convert_timestamp:
+        # return normal records to the user (not converted)
+        return {"success": True, "records": all_db_records}
 
+    # they chose to covert it
+    all_records = []
     for record in all_db_records:
         # convert all the timestamps from strings to timestamp integers
-        # eg 2023-03-26T17:27:17.281892+13:00 -> 1679856072.743881
+        # eg 2023-03-27T21:53:59.713817+13:00 -> 1679907239713
         conv_rec = (await record_pyd.from_tortoise_orm(record)).dict()
-        conv_rec["timestamp"] = record.timestamp.utcnow().timestamp()
-        all_records.append(conv_rec)
+        conv_rec["timestamp"] = record.timestamp.timestamp() * 1000
+        all_records.append(conv_rec)  # add to list of converted records
 
+    # return converted records to the user
     return {"success": True, "records": all_records}
