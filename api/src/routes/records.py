7f8df669c515aa77f58e862537c89a88a89903fd
FusionSid <siddheshadsv@icloud.com>
2023-03-27 21:04:12 +1300

    feat: get records route returns timestamp as an actual number timestamp

--- api/src/routes/records.py
+++ api/src/routes/records.py
@@ -7,6 +7,7 @@ __all__ = ("records_endpoint",)
 from typing import Optional
 
 from fastapi import APIRouter, Request
+from tortoise.contrib.pydantic import pydantic_model_creator  # type: ignore
 
 from core import NewRecord, Record, InvalidRecordID
 
@@ -54,13 +55,26 @@ async def get_existing_record(request: Request, record_id: Optional[int] = None)
     This endpoint gets and existing record from the database
     it can also get all the records from the db
     """
+    record_pyd = pydantic_model_creator(Record, name="Record")
 
     if record_id is not None:
         exists = await Record.exists(id=record_id)
         if not exists:  # if the record doen't exist in the db
             raise InvalidRecordID
         record = await Record.get(id=record_id)
-        return {"success": True, "record": record}
+        converted_record = (await record_pyd.from_tortoise_orm(record)).dict()
+        converted_record["timestamp"] = record.timestamp.utcnow().timestamp()
 
-    all_records = await Record.all()
+        return {"success": True, "record": converted_record}
+
+    all_db_records = await Record.all()
+    all_records = []
+
+    for record in all_db_records:
+        # convert all the timestamps from strings to timestamp integers
+        # eg 2023-03-26T17:27:17.281892+13:00 -> 1679856072.743881
+        conv_rec = (await record_pyd.from_tortoise_orm(record)).dict()
+        conv_rec["timestamp"] = record.timestamp.utcnow().timestamp()
+        all_records.append(conv_rec)
+
     return {"success": True, "records": all_records}
