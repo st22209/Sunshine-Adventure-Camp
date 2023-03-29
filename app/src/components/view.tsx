1b75d525ae66ab73bab1e031f1e994fddb382f7d
FusionSid <siddheshadsv@icloud.com>
2023-03-29 16:19:45 +1300

    feat: add delete button on table viewer

--- app/src/components/view.tsx
+++ app/src/components/view.tsx
@@ -66,7 +66,7 @@ const View = () => {
 				console.log(response);
 				throw new Error();
 			}
-			alert("Record Updated!");
+			alert(response["detail"]);
 			resetModalData();
 		} catch {
 			alert(
@@ -94,6 +94,30 @@ const View = () => {
 			alert("Failed to make a request. Please check api is up.");
 		}
 	};
+
+	const deleteRecord = async (recordId: number) => {
+		if (!window.confirm("Delete the item?")) {
+			return alert("Ok not deleting");
+		}
+		const API_URL = `http://localhost:8443/api/v1/records/?record_id=${recordId}`;
+		try {
+			let res = await fetch(API_URL, {
+				method: "DELETE",
+				headers: {
+					Accept: "application/json",
+					"Content-Type": "application/json",
+				},
+			});
+			let res_data = await res.json();
+			if (res_data["success"] === false) {
+				alert("error api skill issue");
+			}
+			alert(res_data["detail"]);
+		} catch {
+			alert("Failed to make a request. Please check api is up.");
+		}
+	};
+
 	const resetModalData = () => {
 		setModalData({ showModal: false, recordId: null, data: null });
 		setName("");
@@ -138,7 +162,8 @@ const View = () => {
 						>
 							New Record
 						</a>
-						<a href="http://127.0.0.1:8443/api/v1/records/export?convert_timestamp=true"
+						<a
+							href="http://127.0.0.1:8443/api/v1/records/export?convert_timestamp=true"
 							type="button"
 							className="text-white bg-[#F64E60] hover:bg-[#ce3847] font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
 						>
@@ -168,6 +193,7 @@ const View = () => {
 								<th scope="col" className="px-6 py-3">
 									Action
 								</th>
+								<th scope="col" className="px-6 py-3"></th>
 							</tr>
 						</thead>
 						<tbody>
@@ -205,6 +231,30 @@ const View = () => {
 												className="font-medium text-blue-600 hover:underline"
 											>
 												Edit
+											</button>
+										</td>
+										<td>
+											<button
+												onClick={() => {
+													deleteRecord(record.id);
+												}}
+												type="button"
+												className="text-white bg-[#F64E60] hover:bg-[#ce3847] font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2"
+											>
+												<svg
+													xmlns="http://www.w3.org/2000/svg"
+													width="16"
+													height="16"
+													fill="currentColor"
+													viewBox="0 0 16 16"
+												>
+													{" "}
+													<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />{" "}
+													<path
+														fill-rule="evenodd"
+														d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+													/>{" "}
+												</svg>
 											</button>
 										</td>
 									</tr>
